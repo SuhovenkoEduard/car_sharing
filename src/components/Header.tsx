8fee32de3c9fbf22a63452c2471d023e925d4353
Aliaksei Kaliada <alex17koleda@gmail.com>
2022-12-19 15:12:26 +0300

    header bck & login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,22 @@
 import React, { ReactElement } from 'react'
 import { TABS } from '../constants/constants'
 import { OverlayButton } from './OverlayButton'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 import '../scss/Header.scss'
 
 export const Header = (): ReactElement => {
+  const location = useLocation()
+  const { pathname: url } = location
+
+  const localPath = url.slice(1, url.length)
+  console.log(localPath)
+
+  const backgroundImageUrl = `./images/header/${localPath}.png`
+
   const navigate = useNavigate()
   return (
-    <div className="global-header">
+    <div className="global-header" style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
       <div className="tabs">
         {TABS.map((tab, index) => (
           <OverlayButton key={index} {...tab} onClick={() => navigate(tab.navigatePath)}/>
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,33 +1,84 @@
 import React, { ReactElement, useState } from 'react'
-import { FormControl, Checkbox } from '@mui/material'
+import { Checkbox, FormControl } from '@mui/material'
 import { ReactComponent as SocialsSVG } from '../images/socials.svg'
 import { useNavigate } from 'react-router-dom'
 import { CarSharingRoutes } from '../types/types'
 
 import '../scss/SignIn.scss'
 
+interface User {
+  role: string
+  roleName: string
+  login: string
+  password: string
+}
+
+const REGISTERED_USERS: User[] = [
+  {
+    role: 'client',
+    roleName: 'Клиент',
+    login: 'client',
+    password: 'client'
+  },
+  {
+    role: 'employee',
+    roleName: 'Сотрудник',
+    login: 'employee',
+    password: 'employee'
+  },
+  {
+    role: 'manager',
+    roleName: 'Главный менеджер кол. центра',
+    login: 'manager',
+    password: 'manager'
+  }
+]
+
 export const SignIn = (): ReactElement => {
   const navigate = useNavigate()
   const [rememberMe, setRememberMe] = useState<boolean>(true)
+  const [login, setLogin] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit = (): void => {
+    const isAuthorised = REGISTERED_USERS.find((user: User) => user.login === login && user.password === password) !== undefined
+
+    if (isAuthorised) {
+      localStorage.setItem('login', login)
+      localStorage.setItem('password', password)
+      navigate(CarSharingRoutes.home)
+    } else {
+      alert('Неправильный логин или пароль')
+      localStorage.setItem('login', '')
+      localStorage.setItem('password', '')
+    }
+  }
+
   return (
     <div className="sign-in-form-container">
       <div className="sign-in-form">
         <FormControl className="actual-form">
           <div className="header">Авторизация</div>
-          <div className="description">Войдите в свою учетную запись, используя адрес электронной почты и пароль, указанные при регистрации.</div>
+          <div className="description">Войдите в свою учетную запись, используя адрес электронной почты и пароль,
+            указанные при регистрации.
+          </div>
           <div className="inputs">
             <div className="email-container">
               <div className="inputLabel">Почта</div>
-              <input id="email" />
+              <input id="email" value={login} onChange={(event) => {
+                setLogin(event.target.value)
+              }}/>
             </div>
             <div className="password-container">
               <div className="inputLabel">Пароль</div>
-              <input id="password" />
+              <input id="password" value={password} onChange={(event) => {
+                setPassword(event.target.value)
+              }}/>
             </div>
           </div>
           <div className="inputs-remember-me_forgot-password">
             <span>
-              <Checkbox id="remember-me-checkbox" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} />
+              <Checkbox id="remember-me-checkbox" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)}/>
               <label htmlFor="remember-me-checkbox">Запомнить меня</label>
             </span>
             <button type="button" className="highlighted-label">
@@ -35,7 +86,7 @@ export const SignIn = (): ReactElement => {
             </button>
           </div>
           <div className="authorization-button">
-            <button type="button" onClick={() => navigate(CarSharingRoutes.home)}>Авторизоваться</button>
+            <button type="button" onClick={handleSubmit}>Авторизоваться</button>
           </div>
           <div className="sign-up-link-container">
             <span>У вас нет учётной записи?</span>
@@ -46,7 +97,7 @@ export const SignIn = (): ReactElement => {
           <hr style={{ width: '488px', marginLeft: '-48px', border: '1px solid #E5E8ED' }}/>
           <div className="footer">
             <div>Или войдите в систему с помощью</div>
-            <SocialsSVG />
+            <SocialsSVG/>
           </div>
         </FormControl>
       </div>
